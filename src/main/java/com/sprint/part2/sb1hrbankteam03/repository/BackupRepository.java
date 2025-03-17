ea86a38cd939e0621d5ffbbbbf5cb9c16f273b5d
yudility <yudility@gmail.com>
2025-03-17 11:20:03 +0900

    feat: 백업 기능 관련 서비스, 레포지토리 설계

--- src/main/java/com/sprint/part2/sb1hrbankteam03/repository/BackupRepository.java
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/repository/BackupRepository.java
@@ -1,5 +1,32 @@
 package com.sprint.part2.sb1hrbankteam03.repository;
 
-public interface BackupRepository {
+import com.sprint.part2.sb1hrbankteam03.entity.Backup;
+import com.sprint.part2.sb1hrbankteam03.entity.BackupStatus;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BackupRepository extends JpaRepository<Backup, Long> {
+
+  //데이터 백업 이력 중 가장 최신 작업 조회
+  Optional<Backup> findTopByStatusOrderByEndAtDesc(BackupStatus status);
+
+  List<Backup> findByStatusAndWorkerIpContainingAndStartAtBetweenOrderByStartAtDesc(
+      BackupStatus status, String workerIp, LocalDateTime startAtFrom, LocalDateTime startAtTo
+      );
+
+/*  @Query(value = "SELECT b FROM Backup b " +
+      "WHERE b.status = :status " +
+      "and b.startAt between :startedAtFrom and :startedAtTo " +
+      "and b.workerIp like :workerIp" +
+      " and (:cursor IS NULL OR b.startAt < :cursor)"+ //Default
+      "ORDER BY b.startAt DESC") //Default
+  List<Backup> findByStatusOrderByEndAtDesc(
+      @Param("workerIp") String workerIp,
+      @Param("backupStatus") BackupStatus status,
+      @Param("startedAtFrom") LocalDateTime startAtFrom,
+      @Param("startedAtTo") LocalDateTime startAtTo,
+      Pageable pageable);*/
 
 }
