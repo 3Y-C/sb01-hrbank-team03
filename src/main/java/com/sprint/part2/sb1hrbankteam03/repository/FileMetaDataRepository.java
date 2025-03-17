225bc7f549ce28cffc00a0f3f893c157cffc97f5
이용구 <86422079+reflash407@users.noreply.github.com>
2025-03-17 14:44:16 +0900

    refactor : fileStorage 위치 변경

--- src/main/java/com/sprint/part2/sb1hrbankteam03/repository/FileMetaDataRepository.java
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/repository/FileMetaDataRepository.java
@@ -1,5 +1,8 @@
 package com.sprint.part2.sb1hrbankteam03.repository;
 
-public interface FileMetaDataRepository {
+import com.sprint.part2.sb1hrbankteam03.entity.FileMetaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileMetaDataRepository extends JpaRepository<FileMetaData, Long> {
 
 }
--- src/main/java/com/sprint/part2/sb1hrbankteam03/service/EmployeeChangeDetail.java
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/service/EmployeeChangeDetail.java
@@ -1,5 +1,5 @@
 package com.sprint.part2.sb1hrbankteam03.service;
 
-public interface FileStorage {
+public interface EmployeeChangeDetail {
 
 }
