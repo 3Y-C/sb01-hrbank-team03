b37c26f13d69ab6b6477a2d1a41f1634a0b837ed
이용구 <86422079+reflash407@users.noreply.github.com>
2025-03-17 17:13:15 +0900

    delete : 중복 파일 제거

--- src/main/java/com/sprint/part2/sb1hrbankteam03/service/EmployeeChangeDetailImpl.java
+++
@@ -1,6 +0,0 @@
-package com.sprint.part2.sb1hrbankteam03.service;
-
-
-
-public class EmployeeChangeDetailImpl {
-}
--- src/main/java/com/sprint/part2/sb1hrbankteam03/service/EmployeeChangeDetailService.java
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/service/EmployeeChangeDetailService.java
@@ -1,5 +1,5 @@
 package com.sprint.part2.sb1hrbankteam03.service;
 
-public interface EmployeeChangeDetail {
+public interface EmployeeChangeDetailService {
 
 }
