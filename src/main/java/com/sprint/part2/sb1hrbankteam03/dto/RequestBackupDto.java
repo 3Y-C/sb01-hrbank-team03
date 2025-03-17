462af52859d423a7f5928402c069de159d52d633
yudility <yudility@gmail.com>
2025-03-17 11:20:54 +0900

    feat: 백업 기능 API 컨트롤러 구현

---
+++ src/main/java/com/sprint/part2/sb1hrbankteam03/dto/RequestBackupDto.java
@@ -0,0 +1,30 @@
+package com.sprint.part2.sb1hrbankteam03.dto;
+
+import lombok.Getter;
+
+@Getter
+public class RequestBackupDto {
+
+  String worker;
+  String status;
+  String startedAtFrom;
+  String startedAtTo;
+  int idAfter;
+  String cursor;
+  int size;
+  String sortField;
+  String sortDirection;
+
+  public RequestBackupDto(String worker, String status, String startedAtFrom, String startedAtTo,
+      int idAfter, String cursor, int size, String sortField, String sortDirection) {
+    this.worker = worker;
+    this.status = status;
+    this.startedAtFrom = startedAtFrom;
+    this.startedAtTo = startedAtTo;
+    this.idAfter = idAfter;
+    this.cursor = cursor;
+    this.size = size;
+    this.sortField = sortField;
+    this.sortDirection = sortDirection;
+  }
+}
