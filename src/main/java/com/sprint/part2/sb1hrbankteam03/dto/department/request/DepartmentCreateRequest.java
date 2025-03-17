3919195da3b985eb0862fcd17acc9a9860b81f32
Yudility <yudility@gmail.com>
2025-03-17 10:05:28 +0900

    Revert "[Feature/departments] 부서 기능구현"

--- src/main/java/com/sprint/part2/sb1hrbankteam03/dto/department/request/DepartmentCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.part2.sb1hrbankteam03.dto.department.request;
-
-import java.time.LocalDate;
-
-public record DepartmentCreateRequest(
-  String name,
-  String description,
-  LocalDate establishedDate
-){ }
--- src/main/java/com/sprint/part2/sb1hrbankteam03/dto/department/request/DepartmentUpdateRequest.java
+++
@@ -1,9 +0,0 @@
-package com.sprint.part2.sb1hrbankteam03.dto.department.request;
-
-import java.time.LocalDate;
-
-public record DepartmentUpdateRequest(
-    String name,
-    String description,
-    LocalDate establishedDate
-) { }
